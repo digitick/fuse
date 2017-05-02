8216728e2f063ff233458e9df8da09fe30f18c2d
Alexandre Del Vecchio <alexandre.del-vecchio@digitick.com>
2017-05-02 12:03:29 +0200

    Ajout d'une commande et handler SOAP et ajout de test unitaires très basiques Pour la commande.

---
+++ src/Digitick/Foundation/Fuse/Handler/SoapCommandHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Digitick\Foundation\Fuse\Handler;
+
+
+use Digitick\Foundation\Fuse\CircuitBreaker\CircuitBreakerInterface;
+use Digitick\Foundation\Fuse\Command\AbstractCommand;
+use Digitick\Foundation\Fuse\Command\Soap\SoapCommand;
+use SoapClient;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Psr\SimpleCache\CacheInterface;
+
+class SoapCommandHandler extends CommandHandler
+{
+    /** @var  SoapClient */
+    protected $soapClient;
+
+    public function __construct(SoapClient $soapClient, CircuitBreakerInterface $circuitBreaker, CacheInterface $cacheManager = null, LoggerInterface $logger = null)
+    {
+        parent::__construct($circuitBreaker, $cacheManager, $logger);
+        $this->soapClient = $soapClient;
+    }
+
+    public function execute(AbstractCommand $command)
+    {
+        if (!$command instanceof SoapCommand) {
+            $this->log(LogLevel::CRITICAL, "Not a soap command " . $command->getKey());
+            throw new \InvalidArgumentException();
+        }
+        return parent::execute($command);
+    }
+
+    protected function executeCommand(AbstractCommand $command)
+    {
+        $command->setSoapClient ($this->soapClient);
+        return parent::executeCommand($command);
+    }
+
+    /**
+     * @param AbstractCommand $command
+     * @return bool
+     */
+    protected function isCacheable(AbstractCommand $command)
+    {
+        return false;
+    }
+
+}
