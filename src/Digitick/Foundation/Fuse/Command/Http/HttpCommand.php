4f0c57504267ac2587feff899746e950689c33b3
jbrousseauDigitick <33321950+jbrousseauDigitick@users.noreply.github.com>
2017-12-12 17:03:05 +0100

    add PATCH to HTTP Command

--- src/Digitick/Foundation/Fuse/Command/Http/HttpCommand.php
+++ src/Digitick/Foundation/Fuse/Command/Http/HttpCommand.php
@@ -23,7 +23,7 @@ class HttpCommand extends AbstractCommand
     const HTTP_METHOD_GET = 'GET';
     const HTTP_METHOD_POST = 'POST';
     const HTTP_METHOD_PUT = 'PUT';
-    const HTTP_METHOD_PUT = 'PATCH';
+    const HTTP_METHOD_PATCH = 'PATCH';
     const HTTP_METHOD_DELETE = 'DELETE';
 
     const HTTP_SCHEME_HTTP = 'http';
